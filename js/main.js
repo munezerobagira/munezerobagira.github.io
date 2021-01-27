c99cdc8efee482af07b157ea02ce6dad0510f449
sos10e <munezerobagira@gmail.com>
2021-01-27 17:47:58 +0200

    Updated version to version 2

---
+++ js/main.js
@@ -0,0 +1,36 @@
+let container = document.querySelector("#command");
+container.classList.add("typing-cursor", "d-inline");
+let data = {
+  home: "Sostene-browser-Mac-pro: Sostene poto$",
+  options: [
+    {
+      name: "help",
+      details: "cv - list  all my previous work , social - list all social media , github - list all public github repostories,  ".replace(
+        /\,/g,
+        "<br>"
+      ),
+    },
+  ],
+};
+container.innerHTML = data.home;
+setTimeout(
+  function (data) {
+    let span = document.createElement("span");
+    span.classList.add("type");
+    span.innerHTML = " " + data;
+    container.append(span);
+  },
+  3000,
+  data.options[0].name
+);
+setTimeout(
+  function (data) {
+    let span = document.createElement("p");
+    span.classList.add("data");
+    span.innerHTML = data;
+    container.parentElement.append(span);
+    container.classList.remove("typing-cursor");
+  },
+  4000,
+  data.options[0].details
+);
